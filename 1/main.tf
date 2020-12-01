f85c9cb4fe6662b32950cbd47b7ca41b347f5d7f
Vasyl Sharko <Vasyl.Sharko@contracted.pmi.com>
2020-12-01 17:45:46 +0200

    Add another provider

--- 1/main.tf
+++ 1/main.tf
@@ -1,9 +1,17 @@
 provider "aws" {
   profile = "itea"
   region  = "us-east-1"
+  alias   = "east"
+}
+
+provider "aws" {
+  profile = "itea"
+  region  = "us-east-1"
+  alias   = "west"
 }
 
 resource "aws_instance" "main" {
+  provider      = "aws.east"
   ami           = "ami-0947d2ba12ee1ff75"
   instance_type = "t2.micro"
   tags = {
