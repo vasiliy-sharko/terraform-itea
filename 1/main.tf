6d0e027f51f64e8fc732feeb2643a3242e2e19ee
Vasyl Sharko <Vasyl.Sharko@contracted.pmi.com>
2020-12-01 13:19:16 +0200

    Add instance tag in lesson #1

--- 1/main.tf
+++ 1/main.tf
@@ -6,6 +6,9 @@ provider "aws" {
 resource "aws_instance" "main" {
   ami           = "ami-0947d2ba12ee1ff75"
   instance_type = "t2.micro"
+  tags = {
+    Name = "terraform-example"
+  }
 }
 
 resource "aws_eip" "main" {
